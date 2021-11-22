c0e70ac5844cfa856d5abdad85e4a951a6d05d2e
kou.ukai <kou.ukai@cisys.co.jp>
2021-11-23 00:08:24 +0900

    Python出力設定追加

--- openapi/generate.sh
+++ openapi/generate.sh
@@ -1,6 +1,7 @@
 docker run --rm -v "${PWD}/../:/local" openapitools/openapi-generator-cli:v5.3.0 generate \
     -i /local/openapi/openapi.yaml \
     -g python-flask \
+    -c /local/openapi/config-python-flask.json \
     -o /local/server
 
 docker run --rm -v "${PWD}/../:/local" openapitools/openapi-generator-cli:v5.3.0 generate \
